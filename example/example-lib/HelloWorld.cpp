b81f79685e6e1d877baa18dbaa3055ab3373753c
congtung10t2 <congtung10t2@gmail.com>
2021-02-22 20:50:32 +0700

    add demo project

--- example/example-lib/HelloWorld.cpp
+++ example/example-lib/HelloWorld.cpp
@@ -1,6 +1,15 @@
 #include "HelloWorld.hpp"
-
+#include <nunchuk.h>
+using namespace  nunchuk;
 std::string HelloWorld::helloWorld()
 {
+    AppSettings settings;
+    settings.set_chain(Chain::TESTNET);
+    settings.set_hwi_path("bin/hwi");
+    settings.enable_proxy(false);
+    
+    settings.set_testnet_servers({"127.0.0.1:50001"});
+
+    auto nunchuk = MakeNunchuk(settings);
   return std::string("Hello World");
 }
